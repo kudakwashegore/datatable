3b37c60b9f0b702e67d80a0ec9caac2cf03dc47a
Kudakwashe Gore <kudag@teamzol.co.zw>
2016-12-31 15:24:07 +0200

    comments

--- solution.js
+++ solution.js
@@ -1,17 +1,12 @@
 //test data
 var testData_1 = [ { "company_name":"Medline Industries, Inc.", "product":"Benzalkonium Chloride", "price":"481.63" }, { "company_name":"PD-Rx Pharmaceuticals, Inc.", "product":"Alprazolam", "price":"167.62", "fda_date_approved":"02/12/2015" }, { "company_name":"West-ward Pharmaceutical Corp.", "product":"Flumazenil", "fda_date_approved":"23/04/2015" }, { "company_name":"HyVee Inc", "product":"Aspirin", "price":"218.32", "fda_date_approved":"26/07/2015" }, { "company_name":"Aurobindo Pharma Limited", "product":"carisoprodol", "price":"375.58", "fda_date_approved":"28/11/2014" }, { "company_name":"Apotex Corp", "product":"Risperidone", "price":"213.49", "fda_date_approved":"06/11/2015" }, { "company_name":"Unit Dose Services", "product":"Lovastatin", "price":"169.14", "fda_date_approved":"14/09/2015" }, { "company_name":"Jubilant HollisterStier LLC", "product":"Dog Hair Canis spp.", "fda_date_approved":"31/12/2014" }, { "company_name":"AAA Pharmaceutical, Inc.", "product":"ACETAMINOPHEN, CHLORPHENIRAMINE MALEATE, DEXTROMETHORPHAN HYDROBROMIDE, and PHENYLEPHRINE HYDROCHLORIDE", "price":"183.33", "fda_date_approved":"13/12/2015" }, { "company_name":"AKG Innovations LLC", "product":"AVOBENZONE, OCTINOXATE, OCTISALATE", "fda_date_approved":"22/01/2015" }, { "company_name":"hikma Farmaceutica", "product":"Oxytocin" }, { "company_name":"prime Packaging, Inc.", "product":"Avobenzone, Homosalate, Octisalate, Octocrylene, Oxybenzone", "price":"208.17" }, { "company_name":"Davion, Inc", "product":"Triclosan", "price":"80.30", "fda_date_approved":"13/12/2014" }, { "company_name":"CARDINAL HEALTH", "product":"CARBOXYMETHYLCELLULOSE SODIUM, GLYCERIN", "price":"330.22", "fda_date_approved":"11/08/2015" }, { "company_name":"Amgen Inc", "product":"darbepoetin alfa", "price":"332.28", "fda_date_approved":"01/07/2015" }, { "company_name":"Autumn Harp, Inc.", "product":"Salicylic Acid", "price":"34.43", "fda_date_approved":"25/03/2015" }, { "company_name":"American Regent, Inc.", "product":"sodium phosphate, monobasic, monohydrate and sodium phosphate, dibasic anhydrous", "price":"11.60" }, { "company_name":"J. A. Cosmetics U.S. INC", "product":"TITANIUM DIOXIDE", "price":"130.90", "fda_date_approved":"01/12/2015" }, { "company_name":"NATURE REPUBLIC CO., LTD.", "product":"Titanium Dioxide, OCTINOXATE, Zinc Oxide", "price":"124.48" }, { "company_name":"L. Perrigo Company", "product":"Dextromethorphan Hydrobromide, Guaifenesin", "price":"73.09", "fda_date_approved":"03/02/2016" } ];
 
-
-var testData_2 = [{ "first_name": "Billy", "last_name": "Campbell", "phone": "62-(500)527-5325" }, { "first_name": "Jonathan", "last_name": "Black", "country": "Russia", "phone": "7-(729)811-4597" }, { "first_name": "cheryl", "last_name": "Harvey", "country": "Indonesia", "phone": "62-(825)454-3810" }, { "first_name": "Cynthia", "last_name": "Cooper" }, { "first_name": "Thomas", "last_name": "Stevens", "phone": "86-(527)535-8464" }, { "first_name": "Jane", "last_name": "Chavez", "country": "Netherlands" }, { "first_name": "bobby", "last_name": "Price", "country": "China", "phone": "86-(898)723-6749" }, { "first_name": "Steve", "last_name": "Hansen", "phone": "93-(362)494-5552" }, { "first_name": "Alan", "last_name": "Cruz", "country": "Philippines", "phone": "63-(617)248-8832" }, { "first_name": "Dennis", "last_name": "Baker", "country": "Iran", "phone": "98-(436)329-3723" }, { "first_name": "Ernest", "last_name": "Bishop", "phone": "86-(566)429-1138" }, { "first_name": "Russell", "last_name": "Meyer", "phone": "62-(687)827-4302" }, { "first_name": "Ryan", "last_name": "Mendoza", "country": "Poland", "phone": "48-(537)109-0373" }, { "first_name": "Maria", "last_name": "Greene", "phone": "92-(831)367-8049" }, { "first_name": "Elizabeth", "last_name": "Moore", "country": "Philippines", "phone": "63-(694)844-9255" }, { "first_name": "Ronald", "last_name": "kim", "phone": "46-(339)931-9221" }, { "first_name": "Samuel", "last_name": "Jacobs", "country": "Russia", "phone": "7-(936)156-5229" }, { "first_name": "Fred", "last_name": "Ross", "phone": "55-(594)481-7354" }, { "first_name": "Andrew", "last_name": "Burns", "country": "Portugal", "phone": "351-(174)443-8706" }, { "first_name": "Robert", "last_name": "Frazier", "country": "Somalia" }];
-
-
-
-
+//creating a reusable min library to do all the job
 (function(global){
     "use strict";
     
     //define our data table min library in IIF to avoid conflicts with any other libraries
-    function MyDataTable(containerId, data){
+    function MyDataTable(containerId, data){        
             var _this = this;
             _this.server_data = data;               
             _this.tableContainer = document.getElementById(containerId);
@@ -28,9 +23,11 @@ var testData_2 = [{ "first_name": "Billy", "last_name": "Campbell", "phone": "62
             }
     }    
    
+    //function to build and bind data to table
     MyDataTable.prototype.createTable = function(){
         var _this = this;
         
+        //start building the table
         if(_this.headings.length > 0 && _this.tableContainer){           
             var table = document.createElement('table');
             var headerRow = document.createElement('tr');
@@ -55,45 +52,87 @@ var testData_2 = [{ "first_name": "Billy", "last_name": "Campbell", "phone": "62
                 for(var key in _this.headings){
                         td = document.createElement('td'); 
                         text = _this.server_data[i][_this.headings[key]] ? _this.server_data[i][_this.headings[key]] : "";
-                    
+                        
                         td.appendChild(document.createTextNode(text));
                         tr.appendChild(td);
                     } 
                 
+                //current row building done, attach row to table
                 table.appendChild(tr);
             }
 
             
+            //set border to table 
             table.setAttribute('border', '1');
+            
+            //done building table attach to given cintainer
             _this.tableContainer.appendChild(table);
         }
     }
     
-    //work on click event
+    //bind table headers to click event
     MyDataTable.prototype.bindClickSort = function(element, tableElement){
                 var _this = this;
                 element.addEventListener("click", function(){
                     var sort_column = element.textContent;
-
+                    
+                    //sort data according to clicked colums
                     _this.server_data.sort(function(a, b){
                         var firstVal = a[sort_column] ? a[sort_column].toLocaleLowerCase() : "";
                         var seconVal = b[sort_column] ? b[sort_column].toLocaleLowerCase() : "";
-                        return ((firstVal > seconVal) ? -1 : ((firstVal < seconVal) ? 1 : 0));
+                        
+                        //check if sorting list of dates
+                        if(_this.isDate(firstVal) || _this.isDate(seconVal)){
+                            return _this.toInterDateFormat(seconVal) - _this.toInterDateFormat(firstVal);
+                        }                            
+                        //check if not sorting list of numbers
+                        else if(isNaN(firstVal) || isNaN(seconVal))
+                            return ((firstVal > seconVal) ? -1 : ((firstVal < seconVal) ? 1 : 0));
+                        //its a list of numbers
+                        else
+                            return seconVal - firstVal;
                     });   
 
-                    _this.SortTable(tableElement);
+                    _this.sortTable(tableElement);
 
                 });
             }
     
     //recreate table with sorted data
-    MyDataTable.prototype.SortTable = function(tableElement){
+    MyDataTable.prototype.sortTable = function(tableElement){
                 var _this = this;
+                
+                //remove old table
                 _this.tableContainer.removeChild(tableElement);
+                
+                //create a fresh table
                 _this.createTable();
             }
     
-    //attach our MyDataTable object to global object
+    //test if valid date with format DD/MM/YYYY as per test data
+    //but we could test for different expected date formarts here
+    MyDataTable.prototype.isDate = function(date_string){         
+        if(date_string && date_string.match(/^\d{2}\/\d{2}\/\d{4}$/) !== null){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    //convert DD/MM/YYYY date to standard date format MM/DD/YYYY
+    MyDataTable.prototype.toInterDateFormat = function(dateString){        
+        var dateSplit = dateString.split("/");  
+        //check if date is in 3 parts
+        //we can test for a number of cases here to check if dateString is in the format we expect
+        if(dateSplit.length === 3)
+            return new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+        else{            
+            return new Date("0000");
+        }
+    }
+    
+    
+    //attach MyDataTable object to global object
     global.MyDataTable = MyDataTable;
     
 })(window)
@@ -101,5 +140,9 @@ var testData_2 = [{ "first_name": "Billy", "last_name": "Campbell", "phone": "62
 
 
 //now making use of the library
-var dataTable = new MyDataTable("data_table", testData_2);
+
+//create a MyDataTable object
+//if loading from a live server, ajax calls can also be done here and the following code bloack can be wraped inside ajax requests block
+//for testing purposes, will use static testData_1 
+var dataTable = new MyDataTable("data_table", testData_1);
 dataTable.createTable();
